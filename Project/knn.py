cd22f836b51f5c5ce695ebe77543c988873b2121
Parcifal Van Gucht <parcifalvangucht@Parcifals-MacBook-Pro.local>
2022-05-10 13:15:33 +0200

    added pipes for knn and logistic

--- Project/knn.py
+++ Project/knn.py
@@ -1,14 +1,30 @@
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
 import numpy as np
 import Project.main as main
 
-X_train = main.X_train
-y_train = main.y_train
+y_train= main.y_train
 #create a new KNN model
-knn_cv = KNeighborsClassifier(n_neighbors=3)
-#train model with cv of 5
-cv_scores = cross_val_score(knn_cv, X_train, y_train, cv=10)
+knn_cv = KNeighborsClassifier()
+
+results = list()
+neighbors = [5, 10, 20, 50,100]
+scaler = StandardScaler()
+mod_df = scaler.fit_transform(main.mod_df)
+for n in neighbors:
+    # create the modeling pipeline
+    pipeline = Pipeline(steps=[('i', SimpleImputer(strategy='median')), ('m',KNeighborsClassifier(n_neighbors=n))])
+    # evaluate the model
+    strat_kfolds = StratifiedKFold(n_splits=10, shuffle=True, random_state=main.random_state)
+    # evaluate the model and collect the scores
+    n_scores = cross_val_score(pipeline, mod_df, y_train.values.ravel(), scoring='balanced_accuracy', cv=strat_kfolds, n_jobs=-1)
+    # store results
+    results.append(n_scores)
+    print(n, np.mean(n_scores), np.std(n_scores))
+
 #print each cv score (accuracy) and average them
 print(cv_scores)
 print('cv_scores mean:{}'.format(np.mean(cv_scores)))
--- Project/main.py
+++ Project/main.py
@@ -1,18 +1,21 @@
-from sklearn.model_selection import KFold, train_test_split # import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold, train_test_split, cross_val_score, StratifiedKFold # import KFold
 from sklearn.feature_selection import mutual_info_classif
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.linear_model import LogisticRegression
-from sklearn.impute import IterativeImputer
+from sklearn.impute import IterativeImputer, SimpleImputer
+from sklearn.pipeline import Pipeline
 import pandas as pd
 import missingno as mno
 import seaborn as sns
 import numpy as np
+import matplotlib.pyplot as plt
 
-load_path_NISTF = 'Project/data/NISTDB4-F.csv'
-load_path_NISTS = 'Project/data/NISTDB4-S.csv'
-load_path_SFING_Default = 'Project/data/SFinGe_Default.csv'
-load_path_SFING_HQ = 'Project/data/SFinGe_HQNoPert.csv'
-load_path_SFING_VQ = 'Project/data/SFinGe_VQAndPert.csv'
+load_path_NISTF = '/Users/parcifalvangucht/PycharmProjects/BDS_data/NISTDB4-F.csv'
+load_path_NISTS = '/Users/parcifalvangucht/PycharmProjects/BDS_data/NISTDB4-S.csv'
+load_path_SFING_Default = '/Users/parcifalvangucht/PycharmProjects/BDS_data/SFinGe_Default.csv'
+load_path_SFING_HQ = '/Users/parcifalvangucht/PycharmProjects/BDS_data/SFinGe_HQNoPert.csv'
+load_path_SFING_VQ = '/Users/parcifalvangucht/PycharmProjects/BDS_data/SFinGe_VQAndPert.csv'
 raw_data = pd.read_csv(load_path_NISTF, header=None)
 
 random_state= 1
@@ -29,25 +32,72 @@ sns.countplot(data=y_train, x=2014)
 #First handle missing data
 ## Check which data is actually missing and how much data is missing
 attributes_with_missing = np.sum(np.any(np.isnan(X_train), axis=0))
-print('attributes with missing values:', attributes_with_missing)
+print('attributes with missing values starting point:', attributes_with_missing)
 missing_rate = np.count_nonzero(np.isnan(X_train))/np.prod(X_train.shape)
-print('missing value rate', missing_rate)
+print('missing value rate at start', missing_rate)
 # Calculate percentage of missing value per column within dataset
 percent_missing = X_train.isnull().sum() * 100 / len(X_train)
 missing_value_df = pd.DataFrame({'column_name': X_train.columns,
-                                 'percent_missing': percent_missing})
-
-## Imputing missing data through multiple imputation
-imputer = IterativeImputer(random_state=random_state, verbose=2, max_iter=20)
-imputed = imputer.fit_transform(X_train)
+                                 'percent_missing': percent_missing}).sort_values(by=['percent_missing'], ascending=False)
 
 #Check correlations within features (high dimensional data for some datasets p>n)
 corr_mat = X_train.corr()
-mutual_info_classif(X=X_train, y=y_train)
+upper_tri = corr_mat.where(np.triu(np.ones(corr_mat.shape),k=1).astype(bool))
+## columns that contain high correlations
+to_drop_corr = [column for column in upper_tri.columns if any(upper_tri[column].abs() > 0.95)]
+high_corr_mat = corr_mat[to_drop_corr]
+print(to_drop_corr)
+# Check high correlating variables that also have more than 40% missing
+to_drop_missing = missing_value_df[(missing_value_df['percent_missing'] > 40)]
+to_drop_both = to_drop_missing[to_drop_missing['column_name'].isin(to_drop_corr)]
+### No high correlating variables that also have more than 40% missing
 
+## Columns with more than 50% missing data will be deleted
+perc = 40.0
+min_count = int(((100-perc)/100)*X_train.shape[0] + 1)
+mod_df = X_train.dropna( axis=1,
+                thresh=min_count)
+
+missing_rate_mod = np.count_nonzero(np.isnan(mod_df))/np.prod(mod_df.shape)
+print('missing value rate after deletion of missing', missing_rate_mod)
+
+## Columns with high correlation features
+
+## Testing SimpleImputation Strategies with logistic regression: all .88, except
+
+results = list()
+strategies = ['mean', 'median', 'most_frequent', 'constant']
+scaler = StandardScaler()
+mod_df = scaler.fit_transform(mod_df)
+for s in strategies:
+    # create the modeling pipeline
+    pipeline = Pipeline(steps=[('i', SimpleImputer(strategy=s)), ('m', LogisticRegression(multi_class='multinomial', max_iter=500, random_state=random_state))])
+    # evaluate the model
+    strat_kfolds = StratifiedKFold(n_splits=10, shuffle=True, random_state=random_state)
+    # evaluate the model and collect the scores
+    n_scores = cross_val_score(pipeline, mod_df, y_train.values.ravel(), scoring='balanced_accuracy', cv=strat_kfolds, n_jobs=-1)
+    # store results
+    results.append(n_scores)
+    print(s, np.mean(n_scores), np.std(n_scores))
+
+## Median is chosen as SimpleImputation strategy
+
+## Imputing missing data through multiple imputation
+#imputer = IterativeImputer(random_state=random_state, verbose=2, max_iter=20)
+#imputed = imputer.fit_transform(mod_df)
+
+
+
+#high_corr_var = np.where(corr_mat>0.8)
+#high_corr_var = [(corr_mat.columns[row],corr_mat.columns[column]) for row,column in zip(*high_corr_var) if row!=column and row<column]
+
+
+#sns.heatmap(corr_mat, annot=False)
+#plt.show()
+#correlation_pairs = corr_mat.abs().unstack().sort_values(ascending=False)
 
 #listwise deletion
-X_train.dropna()
+#X_train.dropna()
 
 
 #k-fold cross-validation on train set
